d8c6d2f0d9936787fa2332b22b70b3f6185819e1
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-08-15 16:31:29 -0400

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+const hamburgerMenu = document.getElementById("hamburger-icon");
+const navigation = document.getElementById("navigation");
+
+// Open mobile nav menu on hamburger button click
+hamburgerMenu.addEventListener("click", function () {
+	navigation.style.paddingBottom = "100%";
+});
+
+/* If clicking outside of the nav menu, and if the nav menu is already open,
+close the mobile nav menu */
+document.addEventListener("click", function () {
+	if (navigation.clientHeight != "0") {
+		navigation.style.paddingBottom = "0%";
+	}
+});
+
+/* If widow grows larger than 700px wide, hide mobile nav dropdown menu */
+function setBottomPaddingToZero() {
+	if (window.innerWidth > 700) {
+		navigation.style.paddingBottom = "0%";
+	}
+}
+
+window.onresize = setBottomPaddingToZero;
+
+
+// Smoothly scroll to each anchor point when the corresponding link is clicked
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
+		
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		});
+	});
+});
